3f96eb9c4380a80371d3074e88f1f04d90e45735
Andy Wahrenberger <andywahr@microsoft.com>
2019-07-18 15:20:55 -0700

    updates where poi should work thru istio ......

--- scripts/aks-deploy.sh
+++ scripts/aks-deploy.sh
@@ -189,6 +189,70 @@ helm install stable/prometheus-operator --name prometheus-operator --namespace m
 #Add Azure Container Insights (Monitor)
 az aks enable-addons -a monitoring -n $aksname -g $resourcegroup
 
+#Add Istio
+ISTIO_VERSION=1.1.3
+curl -sL "https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istio-$ISTIO_VERSION-linux.tar.gz" | tar xz
+
+cd istio-$ISTIO_VERSION
+sudo cp ./bin/istioctl /usr/local/bin/istioctl
+sudo chmod +x /usr/local/bin/istioctl
+
+# Generate the bash completion file and source it in your current shell
+mkdir -p ~/completions && istioctl collateral --bash -o ~/completions
+source ~/completions/istioctl.bash
+
+# Source the bash completion file in your .bashrc so that the command-line completions
+# are permanently available in your shell
+echo "source ~/completions/istioctl.bash" >> ~/.bashrc
+
+helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
+
+GRAFANA_USERNAME=$(echo -n "grafana" | base64)
+GRAFANA_PASSPHRASE=$(echo -n "REPLACE_WITH_YOUR_SECURE_PASSWORD" | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: grafana
+  namespace: istio-system
+  labels:
+    app: grafana
+type: Opaque
+data:
+  username: $GRAFANA_USERNAME
+  passphrase: $GRAFANA_PASSPHRASE
+EOF
+
+KIALI_USERNAME=$(echo -n "kiali" | base64)
+KIALI_PASSPHRASE=$(echo -n "REPLACE_WITH_YOUR_SECURE_PASSWORD" | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: kiali
+  namespace: istio-system
+  labels:
+    app: kiali
+type: Opaque
+data:
+  username: $KIALI_USERNAME
+  passphrase: $KIALI_PASSPHRASE
+EOF
+
+helm install install/kubernetes/helm/istio --name istio --namespace istio-system \
+  --set global.controlPlaneSecurityEnabled=true \
+  --set mixer.adapters.useAdapterCRDs=false \
+  --set grafana.enabled=true --set grafana.security.enabled=true \
+  --set tracing.enabled=true \
+  --set kiali.enabled=true
+
+#Add Istio to Web and App
+kubectl label namespace web istio-injection=enabled
+kubectl label namespace api istio-injection=enabled
+
+
 #evil Text replacement fun for all 100 yamls
 for file in ../runMe/100*
 do
